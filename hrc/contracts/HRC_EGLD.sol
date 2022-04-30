6d0528bf60a6209cf5ffa65ce8a424f907c45271
Constantin Pricope <constantin.pricope@eu.rsystems.com>
2022-05-01 02:13:46 +0300

    added corrected HRC solidity contract

--- hrc/contracts/HRC_EGLD.sol
+++ hrc/contracts/HRC_EGLD.sol
@@ -1,19 +1,38 @@
+// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
+contract HRC_EGLD is ERC20 {
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    uint256 totalMintedAmount;
+    address owner;
+
+    constructor() ERC20("Wrapped EGLD", "wEGLD")
+    {
+        totalMintedAmount = 0;
+        owner = msg.sender;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
+    function mint(address account, uint256 amount) public{
+
+        require(msg.sender == owner, "Only the owner can mint new tokens");
+        totalMintedAmount = totalMintedAmount + amount;
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount)  public  {
+        totalMintedAmount = totalMintedAmount - amount;
+        _burn(account, amount);
+    }
+
+    function getTotalMintedAmount()  public view  returns (uint256) 
+    {
+        return totalMintedAmount;
+    }
 }
