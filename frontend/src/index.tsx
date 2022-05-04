51535f958cdc96e49bb6512143f9a1ff95b5dd79
Constantin Pricope <constantin.pricope@eu.rsystems.com>
2022-05-05 00:16:02 +0300

    ui fixed details

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src_egld/App';
+import './src_egld/assets/sass/theme.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
