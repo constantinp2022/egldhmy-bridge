52e0c0c65d49af9eb2ac883e05550d593018eb89
Constantin Pricope <constantin.pricope@eu.rsystems.com>
2022-05-06 00:29:46 +0300

    A little bit of design

--- frontend/src/src_egld/components/Layout/Navbar/index.tsx
+++ frontend/src/src_egld/components/Layout/Navbar/index.tsx
@@ -4,7 +4,7 @@ import { Navbar as BsNavbar, NavItem, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { dAppName } from '../../../config';
 import { routeNames } from '../../../routes';
-import { ReactComponent as ElrondLogo } from './../../../assets/img/elrond.svg';
+import { ReactComponent as BridgeLogo } from './../../../assets/img/bridge.svg';
 
 const Navbar = () => {
   const { address } = useGetAccountInfo();
@@ -22,7 +22,7 @@ const Navbar = () => {
           className='d-flex align-items-center navbar-brand mr-0'
           to={isLoggedIn ? routeNames.dashboard : routeNames.home}
         >
-          <ElrondLogo className='elrond-logo' />
+          <BridgeLogo className='bridge-logo' />
           <span className='dapp-name text-muted'>{dAppName}</span>
         </Link>
 
